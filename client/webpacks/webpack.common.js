8df5e2a2f3946af10efaa29aae9486252230f1a5
kirillstepanchuk <kirill.stepanchuk.96@mail.ru>
2022-06-25 22:08:32 +0300

    project template configuration

---
+++ client/webpacks/webpack.common.js
@@ -0,0 +1,64 @@
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const minifyHtmlPluginParameters = {
+  collapseWhitespace: true,
+  removeComments: true,
+  removeRedundantAttributes: true,
+  removeScriptTypeAttributes: true,
+  removeStyleLinkTypeAttributes: true,
+  useShortDoctype: true,
+};
+
+module.exports = () => {
+
+  return {
+    entry: './src/index.tsx',
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: [
+            /node_modules/,
+            /\.test\.(ts|tsx)$/,
+          ],
+          use: ['ts-loader'],
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.(ttf|eot|woff|woff2)$/,
+          loader: 'file-loader',
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['', '.tsx', '.ts', '.js', '.jsx'],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+        minify: minifyHtmlPluginParameters,
+      }),
+    ],
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+          },
+        },
+      },
+    },
+  };
+};
---
+++ client/webpacks/webpack.prod.js
@@ -0,0 +1,11 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common('prod'), {
+  mode: 'production',
+  output: {
+    filename: '[chunkhash].js',
+    path: path.resolve(__dirname, '../dist'),
+  },
+});
